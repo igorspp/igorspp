1b9584d3c27e8dc4faf844c2bc5b461279f1a12a
igorspp <igor.pessi@gmail.com>
2022-03-23 11:15:20 +0200

    Updated

--- old_layout/js/menu-responsive.js
+++
@@ -1,9 +0,0 @@
-function responsiveMenu() {
-  var x = document.getElementById("menu");
-  if (x.className === "menu") {
-    x.className += " responsive"
-  }
-  else {
-  x.className = "menu"
-  }
-}
